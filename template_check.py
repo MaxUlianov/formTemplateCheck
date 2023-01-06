2ceeed8b92de67f30b0af76fe504dbb9bd6943fa
MaxUlianov <curze.curze22@gmail.com>
2023-01-06 22:34:54 +0100

    Finished app functions, added tests with pytest, added README

--- template_check.py
+++ template_check.py
@@ -4,7 +4,6 @@ from typing import Dict
 def template_check(template: Dict, form: Dict):
     """Compares template and form dicts, if all template keys are present in form
     and values match, returns True, else False"""
-    # print(f'\n\nchecking {repr(template)} and {repr(form)}\n')
 
     tkeys = set(template.keys())
     fkeys = set(form.keys())
